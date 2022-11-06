08554892fd4325f83c3261533b30deeb1dc85306
Peter G. Morales <pgmorales76@gmail.com>
2022-11-05 20:28:18 -0400

    Finished functions for problem 1

--- scripts/app_01.js
+++ scripts/app_01.js
@@ -11,9 +11,6 @@ let seattle = {
   min_hourly_customer: 23,
   max_hourly_customer: 65,
   avg_cookie_per_customer: 6.3,
-  sayHello: function () {
-    console.log('Hello from Seattle!');
-  },
   random_number: function (min_hourly_customer, max_hourly_customer, avg_cookie_per_customer) {
     return (Math.floor(randBetween(min_hourly_customer, max_hourly_customer) * avg_cookie_per_customer));
   },
@@ -23,58 +20,54 @@ console.log(seattle.random_number(seattle.min_hourly_customer, seattle.max_hourl
 
 seattle.random_number();
 
-// let tokyo = {
-//   min_hourly_customer: 3,
-//   max_hourly_customer: 24,
-//   avg_cookie_per_customer: 1.2,
-//   sayHello: function() {
-//     console.log('Hello from Tokyo!');
-//   }
-//   random_number: function randBetween(min_hourly_customer, max_hourly_customer) {
-//     return min_hourly_customer + Math.random() * (max_hourly_customer - min_hourly_customer);
-//     console.log(random_number);
-//   }
-// };
+let tokyo = {
+  min_hourly_customer: 3,
+  max_hourly_customer: 24,
+  avg_cookie_per_customer: 1.2,
+  random_number: function (min_hourly_customer, max_hourly_customer, avg_cookie_per_customer) {
+    return (Math.floor(randBetween(min_hourly_customer, max_hourly_customer) * avg_cookie_per_customer));
+  },
+};
 
-// let dubai = {
-//   min_hourly_customer: 11,
-//   max_hourly_customer: 38,
-//   avg_cookie_per_customer: 3.7,
-//   sayHello: function() {
-//     console.log('Hello from Dubai!');
-//   }
-//   random_number: function randBetween(min_hourly_customer, max_hourly_customer) {
-//     return min_hourly_customer + Math.random() * (max_hourly_customer - min_hourly_customer);
-//     console.log(random_number);
-//   }
-// };
+console.log(tokyo.random_number(tokyo.min_hourly_customer, tokyo.max_hourly_customer, tokyo.avg_cookie_per_customer));
 
-// let paris = {
-//   min_hourly_customer: 20,
-//   max_hourly_customer: 38,
-//   avg_cookie_per_customer: 2.3,
-//   sayHello: function() {
-//     console.log('Hello from Paris}!');
-//   }
-//   random_number: function randBetween(min_hourly_customer, max_hourly_customer) {
-//     return min_hourly_customer + Math.random() * (max_hourly_customer - min_hourly_customer);
-//     console.log(random_number);
-//   }
-// };
+tokyo.random_number();
 
-// let lima = {
-//   min_hourly_customer: 2,
-//   max_hourly_customer: 16,
-//   avg_cookie_per_customer: 4.6,
-//   sayHello: function() {
-//     console.log('Hello from Lima!');
-//   }
-//   random_number: function randBetween(min_hourly_customer, max_hourly_customer) {
-//     return min_hourly_customer + Math.random() * (max_hourly_customer - min_hourly_customer);
-//     console.log(random_number);
-//   }
-// };
+let dubai = {
+  min_hourly_customer: 11,
+  max_hourly_customer: 38,
+  avg_cookie_per_customer: 3.7,
+  random_number: function (min_hourly_customer, max_hourly_customer, avg_cookie_per_customer) {
+    return (Math.floor(randBetween(min_hourly_customer, max_hourly_customer) * avg_cookie_per_customer));
+  },
+};
 
-// // function randBetween(min, max) {
-// //   return min + Math.random() * (max - min);
-// // }
+console.log(dubai.random_number(dubai.min_hourly_customer, dubai.max_hourly_customer, dubai.avg_cookie_per_customer));
+
+dubai.random_number();
+
+let paris = {
+  min_hourly_customer: 20,
+  max_hourly_customer: 38,
+  avg_cookie_per_customer: 2.3,
+  random_number: function (min_hourly_customer, max_hourly_customer, avg_cookie_per_customer) {
+    return (Math.floor(randBetween(min_hourly_customer, max_hourly_customer) * avg_cookie_per_customer));
+  },
+};
+
+console.log(paris.random_number(paris.min_hourly_customer, paris.max_hourly_customer, paris.avg_cookie_per_customer));
+
+paris.random_number();
+
+let lima = {
+  min_hourly_customer: 2,
+  max_hourly_customer: 16,
+  avg_cookie_per_customer: 4.6,
+  random_number: function (min_hourly_customer, max_hourly_customer, avg_cookie_per_customer) {
+    return (Math.floor(randBetween(min_hourly_customer, max_hourly_customer) * avg_cookie_per_customer));
+  },
+};
+
+console.log(lima.random_number(lima.min_hourly_customer, lima.max_hourly_customer, lima.avg_cookie_per_customer));
+
+lima.random_number();
