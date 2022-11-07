c290e00748bf137fa044aa83a186922051262b1b
Peter G. Morales <pgmorales76@gmail.com>
2022-11-07 16:41:32 -0500

    Fixed commented out line

--- scripts/app_01.js
+++ scripts/app_01.js
@@ -6,7 +6,7 @@ console.log('Hello, world!');
 // hours of operation
 let hours_of_operation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-// Use a method of that object to generate a random number of customers per hour.
+// Use a method of each city object literal to generate a random number of customers per hour.
 function randBetween(min, max) {
   return min + Math.random() * (max - min);
 }
