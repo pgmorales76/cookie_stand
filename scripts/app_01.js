8d28138518f19ced81d5b13d76653f4f804a879c
Peter G. Morales <pgmorales76@gmail.com>
2022-11-07 16:14:04 -0500

    Added display function for other cities

--- scripts/app_01.js
+++ scripts/app_01.js
@@ -40,7 +40,15 @@ let seattle = {
   total_cookies_sold: 0,
 
   // Display the values of each array as unordered lists in the browser.
-  display_values_for_seattle: function() {
+  // Display the values of each array as unordered lists in the browser.
+  // Remember 4 Steps of DOM Manipulation!
+  // How to Create an HTML Element With JS (Thanks, Profs. Adam & Ben!)
+  // 1. Select the parent element - document.getElementById()
+  // 2. Create a new element - document.createElement()
+  // 3. Fill created element with 'stuff' - .innerText  <--- this is a PROPERTY!!!
+  // 4. Append the created element to the parent element - document.appendChild()
+
+  display_values: function () {
     this.simulated_amounts_of_cookies_purchased();
     let seattle_display = document.getElementById('seattle');
     for (let i = 0; i < hours_of_operation.length; i++) {
@@ -57,7 +65,7 @@ let seattle = {
 console.log(seattle.random_number_of_customers(seattle.min_hourly_customer, seattle.max_hourly_customer, seattle.avg_cookie_per_customer));
 seattle.random_number_of_customers();
 seattle.simulated_amounts_of_cookies_purchased();
-seattle.display_values_for_seattle();
+seattle.display_values();
 
 let tokyo = {
   min_hourly_customer: 3,
@@ -77,11 +85,27 @@ let tokyo = {
       console.log(this.simulated_amounts_of_cookies_purchased_for_tokyo);
     }
   },
+
+  total_cookies_sold: 0,
+
+  display_values: function () {
+    this.simulated_amounts_of_cookies_purchased();
+    let tokyo_display = document.getElementById('tokyo');
+    for (let i = 0; i < hours_of_operation.length; i++) {
+      let list_item_element = document.createElement('li');
+      list_item_element.innerText = `${hours_of_operation[i]}: ${this.simulated_amounts_of_cookies_purchased_for_tokyo}`;
+      tokyo_display.appendChild(list_item_element);
+    }
+    let total_list_item_element = document.createElement('li');
+    total_list_item_element.innerText = `Total: ${this.total_cookies_sold}`;
+    tokyo_display.appendChild(total_list_item_element);
+  }
 };
 
 console.log(tokyo.random_number_of_customers(tokyo.min_hourly_customer, tokyo.max_hourly_customer, tokyo.avg_cookie_per_customer));
 tokyo.random_number_of_customers();
 tokyo.simulated_amounts_of_cookies_purchased();
+tokyo.display_values();
 
 let dubai = {
   min_hourly_customer: 11,
@@ -102,11 +126,27 @@ let dubai = {
       console.log(this.simulated_amounts_of_cookies_purchased_for_dubai);
     }
   },
+
+  total_cookies_sold: 0,
+
+  display_values: function () {
+    this.simulated_amounts_of_cookies_purchased();
+    let dubai_display = document.getElementById('dubai');
+    for (let i = 0; i < hours_of_operation.length; i++) {
+      let list_item_element = document.createElement('li');
+      list_item_element.innerText = `${hours_of_operation[i]}: ${this.simulated_amounts_of_cookies_purchased_for_dubai}`;
+      dubai_display.appendChild(list_item_element);
+    }
+    let total_list_item_element = document.createElement('li');
+    total_list_item_element.innerText = `Total: ${this.total_cookies_sold}`;
+    dubai_display.appendChild(total_list_item_element);
+  }
 };
 
 console.log(dubai.random_number_of_customers(dubai.min_hourly_customer, dubai.max_hourly_customer, dubai.avg_cookie_per_customer));
 dubai.random_number_of_customers();
 dubai.simulated_amounts_of_cookies_purchased();
+dubai.display_values();
 
 let paris = {
   min_hourly_customer: 20,
@@ -127,11 +167,27 @@ let paris = {
       console.log(this.simulated_amounts_of_cookies_purchased_for_paris);
     }
   },
+
+  total_cookies_sold: 0,
+
+  display_values: function () {
+    this.simulated_amounts_of_cookies_purchased();
+    let paris_display = document.getElementById('paris');
+    for (let i = 0; i < hours_of_operation.length; i++) {
+      let list_item_element = document.createElement('li');
+      list_item_element.innerText = `${hours_of_operation[i]}: ${this.simulated_amounts_of_cookies_purchased_for_paris}`;
+      paris_display.appendChild(list_item_element);
+    }
+    let total_list_item_element = document.createElement('li');
+    total_list_item_element.innerText = `Total: ${this.total_cookies_sold}`;
+    paris_display.appendChild(total_list_item_element);
+  }
 };
 
 console.log(paris.random_number_of_customers(paris.min_hourly_customer, paris.max_hourly_customer, paris.avg_cookie_per_customer));
 paris.random_number_of_customers();
 paris.simulated_amounts_of_cookies_purchased();
+paris.display_values();
 
 let lima = {
   min_hourly_customer: 2,
@@ -152,29 +208,24 @@ let lima = {
       console.log(this.simulated_amounts_of_cookies_purchased_for_lima);
     }
   },
+
+  total_cookies_sold: 0,
+
+  display_values: function () {
+    this.simulated_amounts_of_cookies_purchased();
+    let lima_display = document.getElementById('paris');
+    for (let i = 0; i < hours_of_operation.length; i++) {
+      let list_item_element = document.createElement('li');
+      list_item_element.innerText = `${hours_of_operation[i]}: ${this.simulated_amounts_of_cookies_purchased_for_lima}`;
+      lima_display.appendChild(list_item_element);
+    }
+    let total_list_item_element = document.createElement('li');
+    total_list_item_element.innerText = `Total: ${this.total_cookies_sold}`;
+    lima_display.appendChild(total_list_item_element);
+  }
 };
 
 console.log(lima.random_number_of_customers(lima.min_hourly_customer, lima.max_hourly_customer, lima.avg_cookie_per_customer));
 lima.random_number_of_customers();
 lima.simulated_amounts_of_cookies_purchased();
-
-// Display the values of each array as unordered lists in the browser.
-// Remember 4 Steps of DOM Manipulation!
-// How to Create an HTML Element With JS (Thanks, Profs. Adam & Ben!)
-// 1. Select the parent element - document.getElementById()
-// 2. Create a new element - document.createElement()
-// 3. Fill created element with 'stuff' - .innerText  <--- this is a PROPERTY!!!
-// 4. Append the created element to the parent element - document.appendChild()
-
-// let display_values = document.getElementById('orders');
-// console.log('the parentElement is:', display_values);
-
-// let display_values_for_unordered_list_items = document.createElement('li');
-// console.log(display_values_for_unordered_list_items);
-
-// display_values_for_unordered_list_items.innerText = seattle.simulated_amounts_of_cookies_purchased_for_seattle,
-// tokyo.simulated_amounts_of_cookies_purchased_for_tokyo, dubai.simulated_amounts_of_cookies_purchased_for_dubai,
-// paris.simulated_amounts_of_cookies_purchased_for_paris, lima.simulated_amounts_of_cookies_purchased_for_lima; // properties do NOT use parentheses
-// console.log(display_values_for_unordered_list_items.innerText);
-
-// display_values.appendChild(display_values_for_unordered_list_items); // methods DO use parentheses
+lima.display_values();
