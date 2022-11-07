a80e0cebdca9daa65badf13650caaaf088d11e0d
Peter G. Morales <pgmorales76@gmail.com>
2022-11-07 14:14:36 -0500

    Added hours of operation variable

--- scripts/app_01.js
+++ scripts/app_01.js
@@ -3,6 +3,8 @@
 // proof of life
 console.log('Hello, world!');
 
+let hours_of_operation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
 function randBetween(min, max) {
   return min + Math.random() * (max - min);
 }
